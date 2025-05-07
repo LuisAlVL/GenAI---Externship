ae4e676f37898df9bed5dda42a504ed356d8fc5c
Luis Alberto Aldana Villalobos <alberto.aldana.vlobos@gmail.com>
2025-05-07 17:44:22 -0600

    Replace symlink with actual folder

---
+++ Exploring Python Concepts/Task 1.py
@@ -0,0 +1,5 @@
+name = "Peter"
+age = "17"
+height = 5.8
+
+print(f"Hi neighbor! My name is {name}, I am {age} years old and {height} feet tall.")
---
+++ Exploring Python Concepts/Task 2.py
@@ -0,0 +1,9 @@
+num1 = 789
+num2 = 6
+# num1 is the reason 6 never sits next to 7 again
+
+# Here, I used f-strings to substitute the values of num1 and num2 
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
+print(f"The difference between {num1} and {num2} is {num1 - num2}")
+print(f"The product of {num1} and {num2} is {num1 * num2}")
+print(f"The quotient of {num1} divided by {num2} is {num1 / num2}")
